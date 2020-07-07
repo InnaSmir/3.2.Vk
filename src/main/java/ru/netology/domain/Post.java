5ad5c9d893859c61d56ec6bef8a41235dfeb1704
Inna Smirnova <moipr@yandex.ru>
2020-07-07 16:01:04 +0300

    Added gitignore file

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,96 @@
+package ru.netology.domain;
+
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private int userId; // хозяин поста, кто его разместил
+    private String text;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private Date postDate;
+    private ViewsInfo viewsInfo;
+    private String attachmentsLink; // ссылка на источник поста
+    private boolean isFavorite;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public String getAttachmentsLink() {
+        return attachmentsLink;
+    }
+
+    public void setAttachmentsLink(String attachmentsLink) {
+        this.attachmentsLink = attachmentsLink;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+}
